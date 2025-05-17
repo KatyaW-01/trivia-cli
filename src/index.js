50352c2c009df3077eba428a93c70d964ccd055f
Katya Weicht <katya.weicht@gmail.com>
2025-05-17 14:49:14 -0600

    Write outline of main menu function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 //main game logic/functions goes here 
+import { select, input } from "@inquirer/prompts";
+
+async function showMainMenu() {
+  const action = await select({
+    message: "Main Menu",
+    choices: [
+      { name: "Start Quiz", value: "start" },
+      { name: "Check Scores", value: "stats"},
+      { name: "Quit", value: "quit"},
+    ]
+  });
+
+  switch (action) {
+    case "start":
+      await startGame();
+      break;
+    case "stats":
+      showStats();
+      await select({ message: "Press Enter to go back", choices: [{ name: "Back", value: "back" }] });
+      showMainMenu();
+      break;
+    case "quit":
+      console.log("Goodbye!")
+  }
+}
