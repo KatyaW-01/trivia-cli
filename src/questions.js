88c00dd47fc65f2510760f144d856ec8ab9295aa
Katya Weicht <katya.weicht@gmail.com>
2025-05-17 17:59:39 -0600

    Add more quiz questions

--- src/questions.js
+++ src/questions.js
@@ -2,7 +2,10 @@ const quizQuestions = [
   {question: "How many toes do cats typically have on their front paws?", answer: 5},
   {question: "What is the name of the cat in Alice in Wonderland?", answer: "The Cheshire Cat"},
   {question: "When was the first cat video recorded?", answer: 1894},
-  {question: "What is the term for a group of kittens?", answer: "A litter"}
+  {question: "What is the term for a group of kittens?", answer: "Litter"},
+  {question: "All cats are born with what color eyes?", answer: "Blue"},
+  {question: "Which part of a cat is as unique as a human fingerprint?", answer: "Nose"},
+  {question: "How many cats did Abraham Lincoln have in the White House?", answer: 4}
 ]
 
 module.exports = { quizQuestions } //need to use this because package.json doesnt have type: module which would allow for export const quizQuestions and then import {quizQuestions} from filepath
